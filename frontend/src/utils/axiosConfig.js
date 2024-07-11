ff16066c067a94bea889976dede828915b1d78e1
Amanda Charith Senevirathna <131018593+ACSENEVIRATHNA@users.noreply.github.com>
2024-07-11 23:28:19 +0530

    Update axiosConfig.js
    server url update

--- frontend/src/utils/axiosConfig.js
+++ frontend/src/utils/axiosConfig.js
@@ -1,4 +1,4 @@
-export const base_url = "http://localhost:4000/api/";
+export const base_url = "https://chama-computers.onrender.com/api/";
 
 const getTokenFromLocalStorage = localStorage.getItem("customer")
   ? JSON.parse(localStorage.getItem("customer"))
